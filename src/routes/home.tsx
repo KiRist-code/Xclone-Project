32093b66206ea962fb352bc7b4307c88e90242c9
KiRist <37296174+KiRist-code@users.noreply.github.com>
2023-11-23 16:18:21 +0900

    [FEAT]Add Navigator & Tweet Form

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,13 @@
+import { styled } from "styled-components";
 import { auth } from "../firebase";
+import PostTweetForm from "../components/post-tweet-form";
+
+const Wrapper = styled.div``;
 
 export default function Home() {
-  const logout = () => {
-    auth.signOut();
-  };
   return (
-    <h1>
-      <button onClick={logout}>Log Out</button>
-    </h1>
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
   );
 }
